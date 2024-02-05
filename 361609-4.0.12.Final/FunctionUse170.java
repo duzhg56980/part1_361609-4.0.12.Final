7af532ab92fa6803071673283bf2a73fd263d99f
duzhg56980 <guokey1996@gmail.com>
2024-02-05 17:30:21 +0800

    init commit

---
+++ 361609-4.0.12.Final/FunctionUse170.java
@@ -0,0 +1,6 @@
+import io.netty.handler.timeout.IdleState;
+public class FunctionUse170 {
+public void funcUse() {
+IdleState idlestate = new IdleState();
+}
+}
---
+++ 361609-4.0.12.Final/FunctionUse259.java
@@ -0,0 +1,7 @@
+import io.netty.handler.traffic.GlobalTrafficShapingHandler;
+public class FunctionUse259 {
+public void funcUse() {
+GlobalTrafficShapingHandler globaltrafficshapinghandler = new GlobalTrafficShapingHandler();
+globaltrafficshapinghandler.release();
+}
+}
